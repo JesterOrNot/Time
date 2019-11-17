a69757839eb0d0878cb7d20b4e104d6a8474d2f0
Sean Hellum <seanhellum45@gmail.com>
2019-11-17 05:12:23 +0000

    up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from kivy.app import App
 from kivy.uix.label import Label
-import kivy.uix
 class MainApp(App):
     def build(self):
         return Label(text=u"Hello World From Kivy!",color =[0.41, 0.42, 0.74, 1])
