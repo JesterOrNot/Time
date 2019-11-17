7a927d62e81673df291534e3d4967180ac544393
Sean Hellum <seanhellum45@gmail.com>
2019-11-17 04:58:48 +0000

    init

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-
-
-class Frame(Widget):
-    pass
-
-
-class TimerApp(App):
+from kivy.uix.label import Label
+class MyFirstKivyApp(App):
     def build(self):
-        return Frame()
-
-
+        return Label(text="Hello World From Kivy!")
 if __name__ == "__main__":
-    TimerApp().run()
+    MyFirstKivyApp().run()
